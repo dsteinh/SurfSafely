4a3dd7e449b9a5e4189d85c784a2b685e48c908c
Domagoj Steinhauzer <78277473+dsteinh@users.noreply.github.com>
2023-04-02 05:08:13 +0200

    Add user converter and refactor user controller to utilize it.

--- src/main/java/hr/algebra/surfsafly/controller/UserController.java
+++ src/main/java/hr/algebra/surfsafly/controller/UserController.java
@@ -1,5 +1,6 @@
 package hr.algebra.surfsafly.controller;
 
+import hr.algebra.surfsafly.converter.UserConverter;
 import hr.algebra.surfsafly.dto.ApiResponse;
 import hr.algebra.surfsafly.dto.UserDto;
 import hr.algebra.surfsafly.exception.UserNotFoundException;
@@ -23,47 +24,36 @@ public class UserController {
     public static final String ALREADY_EXISTS_ERROR = "username already exists";
     private final UserService userService;
     private final JwtGenerator jwtGenerator;
+    private final UserConverter userConverter;
 
     @PostMapping("/register")
-    public ResponseEntity<ApiResponse> register(@RequestBody UserDto user) {
-        if (userService.getByUsername(user.getUsername()).isPresent()) {
-            return new ResponseEntity<>(ApiResponse.error(user,ALREADY_EXISTS_ERROR), HttpStatus.CONFLICT);
+    public ResponseEntity<ApiResponse> register(@RequestBody UserDto userDto) {
+        if (userService.getByUsername(userDto.getUsername()).isPresent()) {
+            return new ResponseEntity<>(ApiResponse.error(userDto, ALREADY_EXISTS_ERROR), HttpStatus.CONFLICT);
         }
         try {
-            User u = User.builder()
-                    .username(user.getUsername())
-                    .password(user.getPassword())
-                    .firstName(user.getFirstName())
-                    .lastName(user.getLastName())
-                    .email(user.getEmail())
-                    .build();
-            userService.saveUser(u);
-            return new ResponseEntity<>(ApiResponse.ok(user), HttpStatus.CREATED);
+            User user = userConverter.convert(userDto);
+            userService.saveUser(user);
+            return new ResponseEntity<>(ApiResponse.ok(userDto), HttpStatus.CREATED);
         } catch (Exception e) {
-            return new ResponseEntity<>(ApiResponse.error(user, e.getMessage()), HttpStatus.CONFLICT);
+            return new ResponseEntity<>(ApiResponse.error(userDto, e.getMessage()), HttpStatus.CONFLICT);
         }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<ApiResponse> login(@RequestBody UserDto user) {
+    public ResponseEntity<ApiResponse> login(@RequestBody UserDto userDto) {
         try {
-            if (Objects.isNull(user.getUsername()) || Objects.isNull(user.getPassword())) {
+            if (Objects.isNull(userDto.getUsername()) || Objects.isNull(userDto.getPassword())) {
                 throw new UserNotFoundException("Username or Password is Empty");
             }
-            User userData = userService.getUserByUsernameAndPassword(user.getUsername(), user.getPassword());
+            User userData = userService.getUserByUsernameAndPassword(userDto.getUsername(), userDto.getPassword());
             if (Objects.isNull(userData)) {
                 throw new UserNotFoundException("Username or Password is Invalid");
             }
             return new ResponseEntity<>(ApiResponse.ok(jwtGenerator.generateToken(
-                    User.builder()
-                            .username(userData.getUsername())
-                            .password(userData.getPassword())
-                            .firstName(userData.getFirstName())
-                            .lastName(userData.getLastName())
-                            .email(userData.getEmail())
-                            .build())), HttpStatus.OK);
+                    userConverter.convert(userDto))), HttpStatus.OK);
         } catch (UserNotFoundException e) {
-            return new ResponseEntity<>(ApiResponse.error(user, e.getMessage()), HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(ApiResponse.error(userDto, e.getMessage()), HttpStatus.NOT_FOUND);
         }
     }
 }
--- src/main/java/hr/algebra/surfsafly/converter/UserConverter.java
+++ src/main/java/hr/algebra/surfsafly/converter/UserConverter.java
@@ -4,17 +4,19 @@ import hr.algebra.surfsafly.dto.UserDto;
 import hr.algebra.surfsafly.model.User;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
+@Component
 @RequiredArgsConstructor
 public class UserConverter {
 
     private final ModelMapper modelMapper;
 
-    private UserDto convertToDto(User user) {
+    public UserDto convert(User user) {
         return modelMapper.map(user, UserDto.class);
     }
 
-    private User convertToEntity(UserDto userDto) {
+    public User convert(UserDto userDto) {
         return modelMapper.map(userDto, User.class);
     }
 }
