90ae4b288b52ca5e6e4bb22c123bc2b2f1ca7122
dsteinh <domagoj.steinhuazer1@gmail.com>
2023-05-15 10:18:06 +0200

    bugfix UserDto response mismatch.

--- src/main/java/hr/algebra/surfsafly/controller/CurrentUserController.java
+++ src/main/java/hr/algebra/surfsafly/controller/CurrentUserController.java
@@ -1,7 +1,9 @@
 package hr.algebra.surfsafly.controller;
 
+import hr.algebra.surfsafly.converter.UserConverter;
 import hr.algebra.surfsafly.dto.ApiResponseDto;
 import hr.algebra.surfsafly.dto.ChangeUserInformationDto;
+import hr.algebra.surfsafly.dto.UserDto;
 import hr.algebra.surfsafly.exception.UserNotFoundException;
 import hr.algebra.surfsafly.model.User;
 import hr.algebra.surfsafly.service.CurrentUserService;
@@ -19,26 +21,28 @@ public class CurrentUserController {
 
     private final CurrentUserService currentUserService;
     private final UserService userService;
+    private final UserConverter userConverter;
 
     @PostMapping("/update-personal-information")
     public ResponseEntity<ApiResponseDto> changePersonalData(@RequestBody ChangeUserInformationDto dto) throws UserNotFoundException {
         User currentUser = currentUserService.getCurrentUser();
         mapPersonalDataDtoToUser(dto, currentUser);
-        userService.save(currentUser);
-        return ResponseEntity.ok(ApiResponseDto.ok(dto));
+        UserDto userDto = userConverter.convert(userService.save(currentUser));
+        return ResponseEntity.ok(ApiResponseDto.ok(userDto));
     }
 
     @DeleteMapping("/delete-account")
     public ResponseEntity<ApiResponseDto> deleteAccount(@RequestHeader(name = "Authorization") String token) throws UserNotFoundException {
         User currentUser = currentUserService.getCurrentUser();
         userService.anonymizeUser(currentUser, token);
-        return ResponseEntity.ok(ApiResponseDto.ok(currentUser));
+        return ResponseEntity.ok(ApiResponseDto.ok("user deleted"));
     }
 
     @GetMapping
     public ResponseEntity<ApiResponseDto> getCurrentUser() throws UserNotFoundException {
         User currentUser = currentUserService.getCurrentUser();
-        return ResponseEntity.ok(ApiResponseDto.ok(currentUser));
+        UserDto userDto = userConverter.convert(currentUser);
+        return ResponseEntity.ok(ApiResponseDto.ok(userDto));
     }
     private void mapPersonalDataDtoToUser(ChangeUserInformationDto dto, User currentUser) {
         currentUser.setEmail(Objects.nonNull(dto.getNewEmail()) ? dto.getNewEmail() : currentUser.getEmail());
